e0ba693362a01c63a8c72eb522fe9a4524ee500a
SeonJungKim <ksj083895@gmail.com>
2021-02-06 13:27:17 +0500

    LazyVStack

--- SwiftUI_LazyVStack_TUT/ContentView.swift
+++ SwiftUI_LazyVStack_TUT/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("LazyVStack")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.vertical, 50)
+            
+            ScrollView {
+                LazyVStack(alignment: .center) {
+                    ForEach(1...100, id: \.self) { i in
+                        //VStack으로 도형 추가
+                        VStack {
+                            Capsule()
+                                .fill(Color.blue)
+                                .frame(width: 100, height: 50)
+                            Text("Row \(i)")
+                                .foregroundColor(.secondary)
+                                .padding(.all, 10)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
